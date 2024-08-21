63f044405175f9fb3e2231bc2d940179da2955dd
alfa03r <muhammadalfajar218@gmail.com>
2024-08-21 22:04:06 +0700

    Last materi - Container

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,21 +7,76 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("First Flutter App"),
+        title: const Center(
+          child: Text(
+            "Container",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Center(
-            child: Text("Hello World"),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(left: 30, right: 20),
+                color: Colors.red,
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  color: Colors.orange,
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+            ],
           ),
-          Center(
-            child: Text("Hello World"),
-          ),
-          Center(
-            child: Text("Hello World"),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+            ],
           ),
         ],
-      )
+      ),
     );
   }
 }
+
+
+
+
+// class MainPage extends StatelessWidget {
+//   const MainPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: const Text("First Flutter App"),
+//         ),
+//         body: const Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text("Hello  "),
+//             Text("Hello World\n World"),
+//             Text("Hello World"),
+//           ],
+//         ));
+//   }
+// }
